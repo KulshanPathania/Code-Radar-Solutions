f6544f1e736df447e6ca340c1c3293a00acb2cde
KulshanPathania <kulshan1210.becse24@chitkara.edu.in>
2025-01-27 18:49:18 +0530

    Solution to Input and Print a Double

--- Input and Print a Double@@6788d5012db25efec946f942/code.c
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    float a;
+    double a;
     printf("");
-    scanf("%f", &a);
-    printf("You entered: %.4f", a);
+    scanf("%lf", &a);
+    printf("You entered: %.4lf", a);
     return 0;
 }
