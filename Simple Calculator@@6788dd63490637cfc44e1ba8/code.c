de7be9b6b4691a320358c0211ca19717578cba9e
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-02-05 18:30:22 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,7 +13,7 @@ int main() {
     else if(c == '*')
         printf("%d\n", a * b);
     else if(c == '/')
-        printf("%f.0\n", (float)a / b);
+        printf("%.0f\n", (float)a / b);
     else
         printf("error");
 }
