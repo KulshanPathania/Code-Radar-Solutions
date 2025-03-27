a9b14bd99107a58dcf27e89b13a978161eeab191
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-03-27 22:49:17 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -5,112 +5,107 @@ int main() {
     scanf("%d",&n);
     for(int i = 0; i<n; i++){
         for(int j = n-i; j<=n; j++){
-            case 1{
+            case 1:{
                 printf("A");
                 break;
             }
-            case 2{
+            case 2:{
                 printf("B");
                 break;
             }
-            case 3{
+            case 3:{
                 printf("C");
                 break;
             }
-            case 4{
+            case 4:{
                 printf("D");
                 break;
             }
-            case 5{
+            case 5:{
                 printf("E");
                 break;
             }
-            case 6{
+            case 6:{
                 printf("F");
                 break;
             }
-            case 7{
+            case 7:{
                 printf("G");
                 break;
             }
-            case 8{
+            case 8:{
                 printf("H");
                 break;
             }
-            case 9{
+            case 9:{
                 printf("I");
                 break;
             }
-            case 10{
+            case 10:{
                 printf("J");
                 break;
             }
-            case 11{
+            case 11:{
                 printf("K");
                 break;
             }
-
-            case 12{
+            case 12:{
                 printf("L");
                 break;
             }
-            case 13{
+            case 13:{
                 printf("M");
                 break;
             }
-            case 14{
+            case 14:{
                 printf("N");
                 break;
             }
-            case 15{
+            case 15:{
                 printf("O");
                 break;
             }
-            case 16{
+            case 16:{
                 printf("P");
                 break;
             }
-            case 17{
+            case 17:{
                 printf("Q");
                 break;
             }
-            case 18{
+            case 18:{
                 printf("R");
                 break;
             }
-            case 19{
+            case 19:{
                 printf("S");
                 break;
             }
-            case 20{
-                printf("S");
-                break;
-            }
-            case 21{
+            case 20:{
                 printf("T");
                 break;
             }
-            case 22{
+            case 21:{
                 printf("U");
                 break;
             }
-            case 23{
+            case 22:{
                 printf("V");
                 break;
             }
-            case 24{
+            case 23:{
                 printf("W");
                 break;
             }
-            case 24{
+            case 24:{
                 printf("X");
                 break;
             }
-            case 25{
+            case 25:{
                 printf("Y");
                 break;
             }
-            case 26{
+            case 26:{
                 printf("Z");
                 break;
             }
