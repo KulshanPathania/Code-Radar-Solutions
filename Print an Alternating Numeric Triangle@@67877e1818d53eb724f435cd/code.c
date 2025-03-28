10fdfc62c4c45c5ca085053ea2e51dd60f04d10f
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-03-28 16:48:45 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -7,8 +7,94 @@ int main(){
     scanf("%d", &n);
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= i; j++){
+            switch(k){
+                case 1:{
+                    printf("%d", a);
+                    break;
+                }
+                case 2:{
+                    printf("%d", b);
+                    break;
+                }
+                case 3:{
+                    printf("%d", a);
+                    break;
+                }
+                case 4:{
+                    printf("%d", a);
+                    break;
+                }
+                case 5:{
+                    printf("%d", b);
+                    break;
+                }
+                case 6:{
+                    printf("%d", a);
+                    break;
+                }
+                case 7:{
+                    printf("%d", b);
+                    break;
+                }
+                case 8:{
+                    printf("%d", a);
+                    break;
+                }
+                case 9:{
+                    printf("%d", b);
+                    break;
+                }
+                case 10:{
+                    printf("%d", a);
+                    break;
+                }
+                case 11:{
+                    printf("%d", a);
+                    break;
+                }
+                case 12:{
+                    printf("%d", b);
+                    break;
+                }
+                case 13:{
+                    printf("%d", a);
+                    break;
+                }
+                case 14:{
+                    printf("%d", b);
+                    break;
+                }
+                case 15:{
+                    printf("%d", a);
+                    break;
+                }
+                case 16:{
+                    printf("%d", b);
+                    break;
+                }
+                case 17:{
+                    printf("%d", a);
+                    break;
+                }
+                case 18:{
+                    printf("%d", b);
+                    break;
+                }
+                case 19:{
+                    printf("%d", a);
+                    break;
+                }
+                case 20:{
+                    printf("%d", b);
+                    break;
+                }
+                case 21:{
+                    printf("%d", a);
+                    break;
+                }
             printf("%d ", k);
             k++;
+            }
         }
         printf("\n");
     }
