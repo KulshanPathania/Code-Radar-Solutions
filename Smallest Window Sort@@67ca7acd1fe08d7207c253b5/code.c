0f159aaec7964de8e5361c79a73dd49766257032
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-03-29 15:38:01 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,14 +1,14 @@
-void run (int arr[10000],int n){
+void run (int arr[],int n){
     int left = 0;
     int right = n - 1;
-    while (arr[left] <= arr[left + 1]){
+    while (left < n - 1 && arr[left] <= arr[left + 1]){
         left++;
     }
     if (left == n - 1){
         printf("Array is alerady sorted");
     }
 
-    while (arr[right] <= arr[right - 1]){
+    while (right > 0 && arr[right] <= arr[right - 1]){
         right--;
     }
     printf("%d", right - left + 1);
