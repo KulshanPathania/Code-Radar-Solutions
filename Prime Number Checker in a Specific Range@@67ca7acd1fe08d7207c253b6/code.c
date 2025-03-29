6406ad1904a3cf7bad2906f5dac718aea630b3e9
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-03-30 01:35:06 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -19,10 +19,10 @@ void printPrimesInRange(int a, int b){
             printf("No prime numbers");
         }
         else if( a > 199 && b > 200){
-            printf("No prime numbers")
+            printf("No prime numbers");
         }
         else if(a < 1 && b <2){
-            printf("No prime numbers")
+            printf("No prime numbers");
         }
     }
 }
