62e2b859c6d53bc3fb8f3490769b883106b06ae6
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-03-29 17:47:46 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -2,10 +2,10 @@ void caesarCipher (char messager[], int shift, char encrypted[]){
     for (int i = 0; messager[i] != '\0'; i++){
         char ch = messager[i];
         if (ch >= 'A' && ch <= 'Z'){
-            ch = (ch - 'A' + shift) % 26 + 'A'
+            ch = (ch - 'A' + shift) % 26 + 'A';
         }
         else if (ch >= 'a' && ch <= 'z'){
-            ch = (ch - 'a' + shift) % 26 + 'a'
+            ch = (ch - 'a' + shift) % 26 + 'a';
         }
     }
 }
