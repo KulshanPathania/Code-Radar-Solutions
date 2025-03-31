a231647451eb28f520c45a133e114d684d027ea2
Kulshan Pathania <kulshan1210.becse24@chitkara.edu.in>
2025-04-01 04:35:03 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -4,7 +4,7 @@ int majorityelement(int arr[], int num){
     int count = 0;
     for (int i = 0; i < n; i++){
         if (arr[i] == num){
-            count ++  
+            count ++;  
         }
     }
 }
